b7bb9bcf363ddeb676a075c25fd860b08b0c4804
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-01-15 15:37:17 +0100

    Create getComputerChoice function
        the program required a function to automatically make a choice
        among a list of options for the rock paper scissors game.
        getComputerChoice answer this requirement.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello world");
+
+
+// write a function that randomly returns "rock", "paper", "scissors" : getComputerChoice
+
+// getComputerChoice function :
+/*
+    - Functions takes no arg
+    - contains an array of choices
+    - randomly compute an array index
+    - return the value at this index
+*/
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice(){
+    const CHOICES = ['rock', 'paper', 'scissors'];
+    const choiceIndex = getRandomInt(3);
+    
+    return CHOICES[choiceIndex]
+}
