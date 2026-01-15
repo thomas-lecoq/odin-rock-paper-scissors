ad88123deab9f4df6df94839041a7dc5234909f7
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-01-15 17:09:18 +0100

    Add playGame function
        the rock paper scissors game couldn't be played. This function
        add 5 rounds of game (playRound function) and handle the final score
        then announce if the computer or human won (or tie)

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getRandomInt(max) {
 }
 
 // Calculates the machine choice.
-function getComputerChoice(){
+function getComputerChoice() {
     const CHOICES = ['rock', 'paper', 'scissors'];
     const choiceIndex = getRandomInt(3);
     
@@ -15,13 +15,14 @@ function getComputerChoice(){
 }
 
 // Calculates the human choice.
-function getHumanChoice(){
+function getHumanChoice() {
     const CHOICES = {"1": 'rock', "2": 'paper', "3": 'scissors'};
 
+    let choiceIndex;
     let continueChoiceLoop = true;
-    
+
     while (continueChoiceLoop === true) {
-        let choiceIndex = prompt("1- rock, 2- paper, 3-scissors: make your selection (type 1, 2 or 3");
+        choiceIndex = prompt("1- rock, 2- paper, 3-scissors: make your selection (type 1, 2 or 3)");
 
         if (choiceIndex in CHOICES){
             continueChoiceLoop = false;
@@ -33,7 +34,7 @@ function getHumanChoice(){
 }
 
 // plays a single round
-function playRound(humanChoice, computerChoice) {
+function playRound(computerChoice=getComputerChoice(), humanChoice=getHumanChoice()) {
 
     let roundWinner;
     switch(true) {
@@ -73,4 +74,41 @@ function playRound(humanChoice, computerChoice) {
     return roundWinner
 }
 
-console.log(playRound('scissors', 'paper'))
+// play the whole game with a predefined number of 5 rounds
+function playGame(numberOfRound=5) {
+
+    let roundWinner; 
+    let gameWinner;
+
+    console.log(`Welcome to rock, paper, scissors game, let\s go for ${numberOfRound} rounds !`)
+    for (let finishedRounds = 0; finishedRounds < numberOfRound; finishedRounds++) {
+        roundWinner = playRound();
+
+        switch(roundWinner) {
+            case 'Tie':
+                break;
+            case 'Computer':
+                score["computerScore"]++;
+                break;
+            case 'Human':
+                score["humanScore"]++;
+                break;
+        }
+    }
+
+    if (score['computerScore'] === score['humanScore']) {
+        gameWinner = 'Nobody';
+    } else if (score['computerScore'] > score['humanScore']) {
+        gameWinner = 'Computer';
+    } else {
+        gameWinner = 'You';
+    }
+
+    scoreMessage = `After ${numberOfRound} rounds: Computer score: ${score['computerScore'] }, Your score ${score['humanScore']}.`;
+    winnerMessage = `${gameWinner} won !`;
+
+    return console.log(scoreMessage + ' ' + winnerMessage)
+}
+
+// run the game in console
+playGame()
