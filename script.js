303eaae547534272467c9ec9027ad0eb2ab2dfc6
Thomas Lecoq <37746451+thomas-lecoq@users.noreply.github.com>
2026-01-15 15:47:40 +0100

    Add getHumanChoice function
        The game was missing a function where the player can make a choice
        among rock papers of scissors. This function solves this issue.

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-console.log("Hello world");
-
-
-// write a function that randomly returns "rock", "paper", "scissors" : getComputerChoice
-
-// getComputerChoice function :
-/*
-    - Functions takes no arg
-    - contains an array of choices
-    - randomly compute an array index
-    - return the value at this index
-*/
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Calculates the machine choice.
 function getComputerChoice(){
     const CHOICES = ['rock', 'paper', 'scissors'];
     const choiceIndex = getRandomInt(3);
     
     return CHOICES[choiceIndex]
 }
+
+// Calculates the human choice.
+function getHumanChoice(){
+    const CHOICES = {"1": 'rock', "2": 'paper', "3": 'scissors'};
+
+    let continueChoiceLoop = true;
+    
+    while (continueChoiceLoop === true) {
+        let choiceIndex = prompt("1- rock, 2- paper, 3-scissors: make your selection (type 1, 2 or 3");
+
+        if (choiceIndex in CHOICES){
+            continueChoiceLoop = false;
+        } else {
+            console.log("Please select an available option (1, 2, 3).");
+        }
+    }
+    return CHOICES[choiceIndex]
+}
+
+console.log(getHumanChoice())
